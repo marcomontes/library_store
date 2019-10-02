6ecfa7ea498a67f951b4492d9dcfe26c625044ce
Marco Montes <marcomontes@gmail.com>
2019-10-01 22:41:08 -0400

    add routes for API

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :books
+      resources :books, only: [:index, :create, :show, :destroy] do
+        collection do
+          get 'search_by_title'
+          get 'search_by_isbn'
+        end
+      end
     end
   end
 end
